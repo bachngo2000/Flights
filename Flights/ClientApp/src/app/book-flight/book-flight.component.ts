43f29dc32e19acd215b66d7a15a28f0455503ddc
Bach Nguyen-Ngo <bachnguyenngo2000@outlook.com>
2023-04-05 15:56:34 -0500

    navigating the user using router service

--- Flights/ClientApp/src/app/book-flight/book-flight.component.ts
+++ Flights/ClientApp/src/app/book-flight/book-flight.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { FlightService } from './../api/services/flight.service';
 import { FlightRm } from '../api/models';
 
@@ -11,6 +11,7 @@ import { FlightRm } from '../api/models';
 export class BookFlightComponent implements OnInit {
 
   constructor(private route: ActivatedRoute,
+    private router: Router,
     private flightService: FlightService){ }
 
 
@@ -26,8 +27,21 @@ export class BookFlightComponent implements OnInit {
     this.flightId = flightId ?? 'not passed';
 
     this.flightService.findFlight({ id: this.flightId })
-      .subscribe(flight => this.flight = flight)
+      .subscribe(flight => this.flight = flight,
+        this.handleError)
 
+  }
+
+  private handleError = (err: any) => {
+
+    if (err.status == 400 || err.status == 404) {
+      alert("Flight not found!")
+      this.router.navigate(['/search-flights'])
+    }
+
+    console.log("Response Error. Status: ", err.status)
+    console.log("Response Error. Status Text: ", err.statusText)
+    console.log(err)
   }
 
 
