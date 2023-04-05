7a747a0d69bace5d08bfacc35a1d7d65cc5f5e36
Bach Nguyen-Ngo <bachnguyenngo2000@outlook.com>
2023-04-05 14:22:27 -0500

    attribute for returning responses

--- Flights/Controllers/FlightController.cs
+++ Flights/Controllers/FlightController.cs
@@ -76,9 +76,18 @@ namespace Flights.Controllers
         public IEnumerable<FlightRm> Search()
             => flights;
 
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
         [HttpGet("{id}")]
-        public FlightRm Find(Guid id)
-            => flights.SingleOrDefault(f => f.Id == id);
+        public ActionResult<FlightRm> Find(Guid id)
+        {
+            var flight = flights.SingleOrDefault(f => f.Id == id);
+
+            if (flight == null)
+                return NotFound();
+
+            return Ok(flight);
+
+        }
 
     }
 }
