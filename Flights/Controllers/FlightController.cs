097c14a244c461667971e896bf031b23461438c8
Bach Nguyen-Ngo <bachnguyenngo2000@outlook.com>
2023-04-06 14:53:15 -0500

    Creates Entities object in controllers

--- Flights/Controllers/FlightController.cs
+++ Flights/Controllers/FlightController.cs
@@ -3,6 +3,7 @@ using Flights.Domain.Entities;
 using Microsoft.AspNetCore.Mvc;
 using Flights.Dtos;
 using Flights.Domain.Errors;
+using Flights.Data;
 
 namespace Flights.Controllers
 {
@@ -13,62 +14,7 @@ namespace Flights.Controllers
 
         private readonly ILogger<FlightController> _logger;
 
-        static Random random = new Random();
-
-
-        static private Flight[] flights = new Flight[]
-        {
-
-        new (   Guid.NewGuid(),
-                "American Airlines",
-                random.Next(90, 5000).ToString(),
-                new TimePlace("Los Angeles",DateTime.Now.AddHours(random.Next(1, 3))),
-                new TimePlace("Istanbul",DateTime.Now.AddHours(random.Next(4, 10))),
-                    2),
-        new (   Guid.NewGuid(),
-                "Emirates",
-                random.Next(90, 5000).ToString(),
-                new TimePlace("Dubai",DateTime.Now.AddHours(random.Next(1, 10))),
-                new TimePlace("Mumbai",DateTime.Now.AddHours(random.Next(4, 15))),
-                random.Next(1, 853)),
-        new (   Guid.NewGuid(),
-                "British Airways",
-                random.Next(90, 5000).ToString(),
-                new TimePlace("London",DateTime.Now.AddHours(random.Next(1, 15))),
-                new TimePlace("Rome",DateTime.Now.AddHours(random.Next(4, 18))),
-                    random.Next(1, 853)),
-        new (   Guid.NewGuid(),
-                "Singapore Airlines",
-                random.Next(90, 5000).ToString(),
-                new TimePlace("Amsterdam",DateTime.Now.AddHours(random.Next(1, 21))),
-                new TimePlace("Glasgow, Scotland",DateTime.Now.AddHours(random.Next(4, 21))),
-                    random.Next(1, 853)),
-        new (   Guid.NewGuid(),
-                "All Nippon Airways",
-                random.Next(90, 5000).ToString(),
-                new TimePlace("Zurich",DateTime.Now.AddHours(random.Next(1, 23))),
-                new TimePlace("Tokyo",DateTime.Now.AddHours(random.Next(4, 25))),
-                    random.Next(1, 853)),
-        new (   Guid.NewGuid(),
-                "Korean Air",
-                random.Next(90, 5000).ToString(),
-                new TimePlace("Seoul",DateTime.Now.AddHours(random.Next(1, 15))),
-                new TimePlace("Madrid",DateTime.Now.AddHours(random.Next(4, 19))),
-                    random.Next(1, 853)),
-        new (   Guid.NewGuid(),
-                "Turkish Airlines",
-                random.Next(90, 5000).ToString(),
-                new TimePlace("Chicago",DateTime.Now.AddHours(random.Next(1, 55))),
-                new TimePlace("Paris",DateTime.Now.AddHours(random.Next(4, 58))),
-                    random.Next(1, 853)),
-        new (   Guid.NewGuid(),
-                "Air France",
-                random.Next(90, 5000).ToString(),
-                new TimePlace("Moscow",DateTime.Now.AddHours(random.Next(1, 58))),
-                new TimePlace("New York City",DateTime.Now.AddHours(random.Next(4, 60))),
-                    random.Next(1, 853))
-
-    };
+        static private readonly Entities Entities = new Entities();
 
         public FlightController(ILogger<FlightController> logger)
         {
@@ -81,7 +27,7 @@ namespace Flights.Controllers
         [ProducesResponseType(typeof(IEnumerable<FlightRm>), 200)]
         public IEnumerable<FlightRm> Search()
         {
-            var flightRmList = flights.Select(flight => new FlightRm(
+            var flightRmList = Entities.Flights.Select(flight => new FlightRm(
                 flight.Id,
                 flight.Airline,
                 flight.Price,
@@ -101,7 +47,7 @@ namespace Flights.Controllers
         [HttpGet("{id}")]
         public ActionResult<FlightRm> Find(Guid id)
         {
-            var flight = flights.SingleOrDefault(f => f.Id == id);
+            var flight = Entities.Flights.SingleOrDefault(f => f.Id == id);
 
             if (flight == null)
                 return NotFound();
@@ -129,7 +75,7 @@ namespace Flights.Controllers
             
             System.Diagnostics.Debug.WriteLine($"Booking a new flight {dto.FlightId}");
            
-            var flight = flights.SingleOrDefault(f => f.Id == dto.FlightId);
+            var flight = Entities.Flights.SingleOrDefault(f => f.Id == dto.FlightId);
 
             if (flight == null)
                 return NotFound();
---
+++ Flights/Data/Entities.cs
@@ -0,0 +1,67 @@
+﻿using Flights.Domain.Entities;
+
+namespace Flights.Data
+{
+    public class Entities
+    {
+        public IList<Passenger> Passengers = new List<Passenger>();
+
+        static Random random = new Random();
+
+        public Flight[] Flights = new Flight[]
+{
+
+        new (   Guid.NewGuid(),
+                "American Airlines",
+                random.Next(90, 5000).ToString(),
+                new TimePlace("Los Angeles",DateTime.Now.AddHours(random.Next(1, 3))),
+                new TimePlace("Istanbul",DateTime.Now.AddHours(random.Next(4, 10))),
+                    2),
+        new (   Guid.NewGuid(),
+                "Emirates",
+                random.Next(90, 5000).ToString(),
+                new TimePlace("Dubai",DateTime.Now.AddHours(random.Next(1, 10))),
+                new TimePlace("Mumbai",DateTime.Now.AddHours(random.Next(4, 15))),
+                random.Next(1, 853)),
+        new (   Guid.NewGuid(),
+                "British Airways",
+                random.Next(90, 5000).ToString(),
+                new TimePlace("London",DateTime.Now.AddHours(random.Next(1, 15))),
+                new TimePlace("Rome",DateTime.Now.AddHours(random.Next(4, 18))),
+                    random.Next(1, 853)),
+        new (   Guid.NewGuid(),
+                "Singapore Airlines",
+                random.Next(90, 5000).ToString(),
+                new TimePlace("Amsterdam",DateTime.Now.AddHours(random.Next(1, 21))),
+                new TimePlace("Glasgow, Scotland",DateTime.Now.AddHours(random.Next(4, 21))),
+                    random.Next(1, 853)),
+        new (   Guid.NewGuid(),
+                "All Nippon Airways",
+                random.Next(90, 5000).ToString(),
+                new TimePlace("Zurich",DateTime.Now.AddHours(random.Next(1, 23))),
+                new TimePlace("Tokyo",DateTime.Now.AddHours(random.Next(4, 25))),
+                    random.Next(1, 853)),
+        new (   Guid.NewGuid(),
+                "Korean Air",
+                random.Next(90, 5000).ToString(),
+                new TimePlace("Seoul",DateTime.Now.AddHours(random.Next(1, 15))),
+                new TimePlace("Madrid",DateTime.Now.AddHours(random.Next(4, 19))),
+                    random.Next(1, 853)),
+        new (   Guid.NewGuid(),
+                "Turkish Airlines",
+                random.Next(90, 5000).ToString(),
+                new TimePlace("Chicago",DateTime.Now.AddHours(random.Next(1, 55))),
+                new TimePlace("Paris",DateTime.Now.AddHours(random.Next(4, 58))),
+                    random.Next(1, 853)),
+        new (   Guid.NewGuid(),
+                "Air France",
+                random.Next(90, 5000).ToString(),
+                new TimePlace("Moscow",DateTime.Now.AddHours(random.Next(1, 58))),
+                new TimePlace("New York City",DateTime.Now.AddHours(random.Next(4, 60))),
+                    random.Next(1, 853))
+
+};
+
+
+    }
+}
