cb6c283c758f20ca16414574bd18829635cc87b2
Bach Nguyen-Ngo <bachnguyenngo2000@outlook.com>
2023-04-06 22:35:42 -0500

    implemented cancel booking feature in the backend

--- Flights/Controllers/BookingController.cs
+++ Flights/Controllers/BookingController.cs
@@ -40,6 +40,27 @@ namespace Flights.Controllers
             return Ok(bookings);
         }
 
+        [HttpDelete]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(500)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public IActionResult Cancel(BookDto dto)
+        {
+            var flight = _entities.Flights.Find(dto.FlightId);
 
+            var error = flight?.CancelBooking(dto.PassengerEmail, dto.NumberOfSeats);
+
+            if (error == null)
+            {
+                _entities.SaveChanges();
+                return NoContent();
+            }
+
+            if (error is NotFoundError)
+                return NotFound();
+
+            throw new Exception($"The error of type: {error.GetType().Name} occurred while canceling the booking made by {dto.PassengerEmail}");
+        }
     }
 }
