121390596ce8535e463729da420601b489f5ab43
Bach Nguyen-Ngo <bachnguyenngo2000@outlook.com>
2023-04-10 08:31:06 -0500

    added FlightSearchParameters.cs class

---
+++ Flights/Dtos/FlightSearchParameters.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel;
+
+namespace Flights.Dtos
+{
+    public record FlightSearchParameters
+    (
+
+        [DefaultValue("04/10/2023 10:30:00 AM")]
+        DateTime? FromDate,
+
+        [DefaultValue("04/11/2023 10:30:00 AM")]
+        DateTime? ToDate,
+
+        [DefaultValue("Los Angeles")]
+        string? From,
+
+        [DefaultValue("Washington, DC.")]
+        string? Destination,
+
+        [DefaultValue(1)]
+        int? NumberOfPassengers
+
+     );
+
+}
